824aa31483601ab94a1d3115267433c67b5e3356
Eli Strauss <straussed@gmail.com>
2017-05-08 22:50:40 -0400

    permutation test of effect of AI on change magnitude + some experimentation with aniDom

--- qualities of alphas.R
+++
@@ -1,9 +0,0 @@
-##################################################################
-##                        Eli Strauss                           ##
-##                    Qualities of alphas                       ##
-##                    January 11th, 2017                        ##
-##################################################################
-
-
-source('~/Documents/Fisibase/ReadTidyAggs.R')
-alphas <- filter(ranks, Rank == 1)
